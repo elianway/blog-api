745daa593eaba7f08a14e62b9424225d7b361476
Eli Anway <elianway@Elis-MBP.cable.rcn.com>
2022-10-30 20:02:49 -0400

    Update to-do

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 //have to create an account to post
 //don't need an account to comment
+//need to add routes and controllers
 //need to add support for comments to model schema
 //need to come up with auth strategy
